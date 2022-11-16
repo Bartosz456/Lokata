cf0f8b1774cdd786f398776214bfe07fc2773212
Bartosz456 <bartoszsz14@gmail.com>
2022-11-16 21:04:33 +0100

    lokata

---
+++ lokata.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+double lokata(double wplata)
+{
+	return (wplata*0.08)*0.81;
+}
+int main()
+{
+	double wplata;
+	cout << "Podaj ile pieniedzy wplacasz: ";
+	cin >> wplata;
+	cout <<"Twoj zysk po roku: " << lokata(wplata);
+}
